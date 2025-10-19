92f94fa12b15960d553776f43d1648d3ab661451
lRasputinl <vlad.zaytsev.06@bk.ru>
2025-10-19 23:54:01 +0300

    Добавлена система здоровья персонажа (#4)
    * Добавлена система здоровья персонажа
    
    * Исправлены недочёты от 19.10.2025 21:33
    
    * Исправлены недочеты от 19.10.2025 23:17
    
    * smallFixes
    
    ---------
    
    Co-authored-by: chsnkoff <chsnk.off@gmail.com>

---
+++ Assets/_Project/Code/Features/Character/MB/CharacterHealthSystem.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System;
+using _Project.Code.Features.Character.MB;
+
+namespace CharacterSystems
+{
+    public class CharacterHealthSystem : MonoBehaviour, IHealthSystem
+    {
+        [SerializeField] private float _maxHealth = 100f;
+        [SerializeField] private Character _character;
+        private float _currentHealth;
+
+        public float CurrentHealth
+        {
+            get => _currentHealth;
+            private set
+            {
+                _currentHealth = Mathf.Clamp(value, 0, _maxHealth);
+                OnHealthChanged?.Invoke(_currentHealth, _maxHealth);
+
+                if (_currentHealth == 0)
+                {
+                    OnDeath?.Invoke();
+                }
+            }
+        }
+
+        public float MaxHealth => _maxHealth;
+
+        public event Action<float, float> OnHealthChanged;
+        public event Action OnDeath;
+
+        private void Awake()
+        {
+            _character.TryRegisterSystem<IHealthSystem>(this);
+            CurrentHealth = _maxHealth;
+        }
+
+        public void TakeDamage (float value)
+        {
+            if (value <= 0)
+            {
+                return;
+            }
+
+            CurrentHealth -= value;
+        }
+
+        public void AddHealth(float value)
+        {
+            if (value <= 0)
+            {
+                return;
+            }
+
+            CurrentHealth += value;
+        }
+    }
+}
---
+++ Assets/_Project/Code/Features/Character/MB/IHealthSystem.cs
@@ -0,0 +1,18 @@
+using System;
+using _Project.Code.Features.Character.MB;
+
+namespace CharacterSystems
+{
+    public interface IHealthSystem : ICharacterSystem
+    {
+        float CurrentHealth { get; }
+        float MaxHealth { get; }
+
+        event Action<float, float> OnHealthChanged;
+        event Action OnDeath;
+
+        void TakeDamage(float value);
+        void AddHealth(float value);
+    }
+}
+
