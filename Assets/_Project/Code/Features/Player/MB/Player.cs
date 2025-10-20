1e604f7cc96f88b693f611d391bf1c098e06c5bc
chsnkoff <chsnk.off@gmail.com>
2025-10-20 15:22:31 +0300

    add: Player Class

---
+++ Assets/_Project/Code/Features/Player/MB/Player.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace _Project.Code.Features.Player.MB
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private Character.MB.Character _character;
+    }
+}
