e7aadcbc718d54551be48a6114633e6c7ab7524e
NotLastTry <artyomn_2005@mail.ru>
2025-10-22 17:01:57 +0300

    Feature: world base
    Добавил контракты, scriptableObject ответственный за конфигурацию мира (размер чанка, сетка, сид).

--- Assets/_Project/Code/Features/Generation/Chunks/WorldConfig.cs
+++ Assets/_Project/Code/Features/Generation/Chunks/WorldConfig.cs
@@ -1,21 +1,21 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu(fileName = "WorldConfig", menuName = "Game/World Config")]
 public class WorldConfig : ScriptableObject
 {
     [Header("Размер чанка (в мировых единицах)")]
-    [Range(10, 1000)]
+    [Range(1, 1000)]
     public float chunkWidth = 100f;
 
-    [Range(10, 1000)]
+    [Range(1, 1000)]
     public float chunkHeight = 100f;
 
     [Header("Сетка чанков")]
-    [Range(4, 2048)]
+    [Range(1, 2048)]
     public int chunksX = 360; // по долготе
-    [Range(2, 1024)]
+    [Range(1, 1024)]
     public int chunksY = 180; // по широте
 
     [Header("Параметры генерации мира")]
-    public int seed = 12345;
+    public int seed = 0;
 }
