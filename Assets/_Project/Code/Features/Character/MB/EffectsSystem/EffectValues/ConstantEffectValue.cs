693e4866ce6855926cc16b7ce047ca426782b1ea
chsnkoff <chsnk.off@gmail.com>
2025-10-31 15:27:56 +0300

    add: ConstantEffectValue

---
+++ Assets/_Project/Code/Features/Character/MB/EffectsSystem/EffectValues/ConstantEffectValue.cs
@@ -0,0 +1,19 @@
+﻿namespace _Project.Code.Features.Character.MB.EffectsSystem.EffectValues
+{
+    public class ConstantEffectValue<T> : ICharacterEffectValue<T>
+    {
+        private T _value;
+        
+        public ConstantEffectValue(T value)
+        {
+            _value = value;
+        }
+        
+        public void Tick(float dt) { }
+
+        public T GetValue()
+        {
+            return _value;
+        }
+    }
+}
