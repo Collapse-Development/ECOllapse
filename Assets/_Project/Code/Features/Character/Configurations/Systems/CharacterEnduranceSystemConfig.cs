519bae35903a2048020904998089a8600409701b
chsnkoff <chsnk.off@gmail.com>
2026-04-20 10:15:33 +0300

    add: endauranceSystemBase

---
+++ Assets/_Project/Code/Features/Character/Configurations/Systems/CharacterEnduranceSystemConfig.cs
@@ -0,0 +1,20 @@
+using _Project.Code.Features.Character.MB.EnduranceSystem;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New CharacterEnduranceSystemConfig", menuName = "Scriptable Objects/Character/Systems/Endurance/CharacterEnduranceSystem")]
+public class CharacterEnduranceSystemConfig : CharacterSystemConfig<CharacterEnduranceSystem>
+{
+    [Header("Endurance")]
+    public float MaxValue = 100f;
+    public float CurrentValue = 100f;
+    public bool StartFromMaxValue = true;
+
+    [Tooltip("How much CurrentValue decreases per second while character is running.")]
+    public float DecreasePerSecond = 10f;
+
+    [Tooltip("How much CurrentValue restores per second while character is not running.")]
+    public float RestorePerSecond = 15f;
+
+    [Tooltip("Delay in seconds before restoration starts after running ends.")]
+    public float RestoreDelay = 1f;
+}
